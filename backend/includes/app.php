8c3f020376a716996f0d58f73035758e882d9bd3
juan-beltranos <sebastianbeltran341@gmail.com>
2022-03-15 21:38:39 -0500

    Backend - Habilitar cors

--- backend/includes/app.php
+++ backend/includes/app.php
@@ -4,6 +4,9 @@ require 'funciones.php';
 require 'database.php';
 require __DIR__ . '/../vendor/autoload.php';
 
+// Habilitar cors
+header('Access-Control-Allow-Origin: *');
+
 // Conectarnos a la base de datos
 use Model\ActiveRecord;
 ActiveRecord::setDB($db);
